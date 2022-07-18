a0bea44e36f2623b793126503840f1a122b8d3c7
rominirani <romin.k.irani@gmail.com>
2022-07-18 09:57:12 +0530

    additional inventory api methods

--- python-flask-api/app.py
+++ python-flask-api/app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import os
 import logging
 from flask import Flask, jsonify, request
@@ -7,6 +8,8 @@ import threading
 iter = itertools.count(start=1,step=1)
 app = Flask(__name__)
 app.logger.setLevel(logging.DEBUG)
+
+inventory_items = {"I-1":10, "I-2":20,"I-3":30}
 
 @app.route('/greeting')
 def greeting():
@@ -19,6 +22,18 @@ def greeting():
         value = next(iter)
 
     return jsonify({'id':value,'content':name})
+
+@app.route('/inventory', methods=['GET'])
+def inventorylist():
+   return jsonify(inventory_items)
+
+@app.route('/inventory/<string:productid>', methods=['GET'])
+def inventory(productid):
+   qty = inventory_items.get(productid)
+   if qty == None:
+    productid='Invalid Product Id'
+    qty = 0
+   return jsonify({'productid':productid,'qty':qty})
 
 
 if __name__ == '__main__':
