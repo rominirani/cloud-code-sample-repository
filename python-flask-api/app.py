45318f0aadebf99d7a9988f452de8b181101b297
Romin Irani <romin.k.irani@gmail.com>
2022-07-19 11:24:11 +0530

    removed greeting request and required libraries

--- python-flask-api/app.py
+++ python-flask-api/app.py
@@ -1,27 +1,11 @@
-from crypt import methods
 import os
 import logging
+import json
 from flask import Flask, jsonify, request
-import itertools
-import threading
 
-iter = itertools.count(start=1,step=1)
 app = Flask(__name__)
-app.logger.setLevel(logging.INFO)
 
 inventory_items = {"I-1":10, "I-2":20,"I-3":30}
-
-@app.route('/greeting')
-def greeting():
-    name = request.args.get('name')
-    if name == None:
-        name = "World"
-    name = "Hello {name}".format(name=name)
-    
-    with threading.Lock():
-        value = next(iter)
-
-    return jsonify({'id':value,'content':name})
 
 @app.route('/healthy', methods=['GET'])
 def healthy():
@@ -35,11 +19,9 @@ def inventorylist():
 def inventory(productid):
    qty = inventory_items.get(productid)
    if qty == None:
-    productid='Invalid Product Id'
-    qty = 0
     app.logger.warning("Received inventory request for incorrect productid:{}".format(productid))
+    qty = -1
    return jsonify({'productid':productid,'qty':qty})
-
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
