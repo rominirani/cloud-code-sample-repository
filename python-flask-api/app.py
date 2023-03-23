e6c7dcd8112589e32b9322f3285f5ee18012d852
Romin Irani <romin.k.irani@gmail.com>
2023-03-23 16:34:53 +0530

    Inject delay and uptime check failure

--- python-flask-api/app.py
+++ python-flask-api/app.py
@@ -1,14 +1,26 @@
 import os
 import logging
 import json
+import time
+import random
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 inventory_items = {"I-1":10, "I-2":20,"I-3":30}
+introduce_delay = False
+introduce_uptimetimecheckfailure = False
+
+if "delay" in os.environ:
+   introduce_delay = os.environ["delay"]
+
+if "uptimecheckfailure" in os.environ:
+   introduce_uptimetimecheckfailure = os.environ["uptimecheckfailure"]
 
 @app.route('/healthy', methods=['GET'])
 def healthy():
+    if introduce_uptimetimecheckfailure:
+       return "Error",500
     return "All Izz Well"
 
 @app.route('/inventory', methods=['GET'])
@@ -18,6 +30,10 @@ def inventorylist():
 @app.route('/inventory/<string:productid>', methods=['GET'])
 def inventory(productid):
    qty = inventory_items.get(productid)
+   #Introduce a delay, if it has been setup in the variable
+   if introduce_delay:
+      time.sleep(random.random())
+
    if qty == None:
     app.logger.warning("Received inventory request for incorrect productid:{}".format(productid))
     qty = -1
